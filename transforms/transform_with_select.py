603aca494f74aa877cda2b4e574c051bbb1421f1
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-07 22:15:50 -0500

    add comment

--- transforms/transform_with_select.py
+++ transforms/transform_with_select.py
@@ -2,5 +2,7 @@ from functions import math
 from package import Engine
 
 def transform(engine: Engine):
+    # select returns a pandas object
     country = engine.select(f'select * from customer order by {math.randInt()} limit 1').iloc[0].Country
+
     return f"select * from customer where country = '{ country }'"

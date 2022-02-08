7c2c93b4712bd0bd8ffbc997e3970b8748946451
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-07 20:32:04 -0500

    fix args

--- project.py
+++ project.py
@@ -1,6 +1,8 @@
 from package import Engine
-from transforms import transform
+from transforms import transform, transformer
 
 engine = Engine()
 first_transform = engine.transform(transform)
 engine.head(first_transform)
+second_transform = engine.transform(second_transform)
+engine.head(second_transform)

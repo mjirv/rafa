0acfa2d3a77924821f3150adfadd8c91abcb3fcc
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-08 21:07:41 -0500

    rename

--- sample_project/transforms/transform_with_select.py
+++ sample_project/transforms/transform_with_select.py
@@ -2,6 +2,7 @@ from functions import math
 
 def transform(jules):
     # select returns a pandas object
+    print(jules.name)
     country = jules.select(f'select * from customer order by {math.randInt()} limit 1').iloc[0].Country
 
     return f"select * from customer where country = '{ country }'"

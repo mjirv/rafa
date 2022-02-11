95fedf07b7c7dcb025826cfe95a42a1a52edc456
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-10 22:11:53 -0500

    starts improving sample project

--- sample_project/project.py
+++ sample_project/project.py
@@ -1,14 +1,20 @@
-from functions import math
 from rafa import *
-from transforms import transform, transform_with_select
+# TODO add imports
 
-# A typical transformation
-first_transform = rafa.transform(transform)
+# Register sources
+src_invoice_items = rafa.source('invoice_items')
+src_customers = rafa.source('customers')
+src_employees = rafa.source('employees')
+src_invoices = rafa.source('invoices')
+src_invoice_items = rafa.source('invoice_items')
 
-# You can run select statements here too
-random_sample = rafa.select(f"select * from { first_transform } order by { math.randInt() } limit 10")
-print(random_sample)
+# Create revenue tables
+invoices = rafa.transform(invoices, sources={src_invoices, src_invoice_items})
+monthly_revenue = rafa.transform(revenue, name='monthly_revenue', period='month')
+weekly_revenue = rafa.transform(revenue, name='weekly_revenue', period='week')
 
-# Or even run a select inside a transformation by passing in the engine as a parameter
-third_transform = rafa.transform(transform_with_select, rafa=rafa)
-rafa.head(third_transform)
+# Create employee revenue table - transforms can be dynamic based on inputs
+customers = rafa.transform(customers, sources={src_customers})
+monthly_revenue_by_support_rep = rafa.transform(revenue, sources={invoices, customers, src_employees}, name='monthly_revenue_by_support_rep', period='month')
+
+# TODO should wee include any select statements?

152b93bb321ba606f8dd355abe2fa98b4f08df34
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-15 23:21:43 -0500

    converts to dictionaries

--- sample_project/project.py
+++ sample_project/project.py
@@ -2,19 +2,18 @@ from rafa import *
 # TODO add imports
 
 # Register sources
-src_invoice_items = rafa.source('invoice_items')
 src_customers = rafa.source('customers')
 src_employees = rafa.source('employees')
 src_invoices = rafa.source('invoices')
 src_invoice_items = rafa.source('invoice_items')
 
 # Create revenue tables
-invoices = rafa.transform(invoices, sources={src_invoices, src_invoice_items})
-monthly_revenue = rafa.transform(revenue, name='monthly_revenue', period='month')
-weekly_revenue = rafa.transform(revenue, name='weekly_revenue', period='week')
+invoices = rafa.transform(invoices, sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
+monthly_revenue = rafa.transform(revenue, name='monthly_revenue', period='month', sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
+weekly_revenue = rafa.transform(revenue, name='weekly_revenue', period='week', sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
 
 # Create employee revenue table - transforms can be dynamic based on inputs
-customers = rafa.transform(customers, sources={src_customers})
-monthly_revenue_by_support_rep = rafa.transform(revenue, sources={invoices, customers, src_employees}, name='monthly_revenue_by_support_rep', period='month')
+customers = rafa.transform(customers, sources={"customers": src_customers})
+monthly_revenue_by_support_rep = rafa.transform(revenue, sources={"invoices": invoices, "customers": customers, "employees": src_employees}, name='monthly_revenue_by_support_rep', period='month')
 
 # TODO should wee include any select statements?

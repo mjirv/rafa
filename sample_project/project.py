45dd7433bad1bdc95d445cd6ffddc36b4d3565e3
Michael Irvine <michael.j.irvine@gmail.com>
2022-02-16 19:34:28 -0500

    add scaffolding for trasnform files

--- sample_project/project.py
+++ sample_project/project.py
@@ -1,19 +1,21 @@
 from rafa import *
-# TODO add imports
+from transforms import customers, invoices, revenue
 
 # Register sources
 src_customers = rafa.source('customers')
 src_employees = rafa.source('employees')
 src_invoices = rafa.source('invoices')
 src_invoice_items = rafa.source('invoice_items')
+revenue_sources = {"invoices": src_invoices, "invoice_items": src_invoice_items}
+
+m_customers = rafa.transform(customers, sources={"customers": src_customers})
 
 # Create revenue tables
-invoices = rafa.transform(invoices, sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
-monthly_revenue = rafa.transform(revenue, name='monthly_revenue', period='month', sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
-weekly_revenue = rafa.transform(revenue, name='weekly_revenue', period='week', sources={"invoices": src_invoices, "invoice_items": src_invoice_items})
+m_invoices = rafa.transform(invoices, sources=revenue_sources)
+m_monthly_revenue = rafa.transform(revenue, name='monthly_revenue', period='month', sources=revenue_sources)
+m_weekly_revenue = rafa.transform(revenue, name='weekly_revenue', period='week', sources=revenue_sources)
 
 # Create employee revenue table - transforms can be dynamic based on inputs
-customers = rafa.transform(customers, sources={"customers": src_customers})
-monthly_revenue_by_support_rep = rafa.transform(revenue, sources={"invoices": invoices, "customers": customers, "employees": src_employees}, name='monthly_revenue_by_support_rep', period='month')
+m_monthly_revenue_by_support_rep = rafa.transform(revenue, sources={"invoices": m_invoices, "customers": m_customers, "employees": src_employees}, name='monthly_revenue_by_support_rep', period='month')
 
 # TODO should wee include any select statements?
